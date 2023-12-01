8d1a668de8537625d6f74e556c7640cf88c70ab3
luuk0406 <luukf0406@gmail.com>
2023-12-01 14:31:03 +0100

    add configure services file

--- InventorySystemAPI/Program.cs
+++ InventorySystemAPI/Program.cs
@@ -1,11 +1,10 @@
+using InventorySystemAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInventorySystemApi();
 
 var app = builder.Build();
 
