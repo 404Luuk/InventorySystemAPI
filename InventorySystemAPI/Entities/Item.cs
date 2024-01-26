d5696e15066213783a429b1e50a37f1f8c6e4ff3
tychovr <68756557+tychovr@users.noreply.github.com>
2024-01-26 13:28:20 +0100

    refactor: renamed item entity to new design

--- InventorySystemAPI/Entities/Item.cs
+++ InventorySystemAPI/Entities/Item.cs
@@ -10,6 +10,6 @@ public class Item
     [MaxLength(30, ErrorMessage = "Maximum length may not exceed 30 chars")]
     public string? ItemName { get; set; }
     public Availability ItemAvailability { get; set; }
-    public string? ItemOwner { get; set; } //  possibly change to user object when implemented
-    public string? ItemCurrentHolder { get; set; } //name is tacky
+    public string? ItemNotes { get; set; }
+    public string? ItemImage { get; set; }
 }
